0cd1eacf7e7e1e86b876cba9b0cc9a59b3572d55
Admin <admin@Admins-MacBook-Air.local>
2025-06-04 19:39:51 +0530

    Feat: Frist commit

---
+++ App.js
@@ -0,0 +1,26 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement(
+    "div",
+    {
+      id: "child",
+    },
+    [
+      React.createElement("h1", { id: "heading" }, "This is h1 Tag!"),
+      React.createElement("h2", { id: "heading" }, "This is h2 Tag!"),
+    ]
+  ),
+  React.createElement(
+    "div",
+    {
+      id: "child",
+    },
+    [
+      React.createElement("h1", { id: "heading" }, "This is h1 Tag!"),
+      React.createElement("h2", { id: "heading" }, "This is h2 Tag!"),
+    ]
+  ),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
